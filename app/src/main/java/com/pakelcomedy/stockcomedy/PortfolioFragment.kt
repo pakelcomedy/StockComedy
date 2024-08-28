c40a311305099d42b4fb97c1074ed3c3ddb3a44f
Cursed Pakel <danieldna1411@gmail.com>
2024-08-28 22:02:12 +0700

    fix portofolio

--- app/src/main/java/com/pakelcomedy/stockcomedy/PortfolioFragment.kt
+++ app/src/main/java/com/pakelcomedy/stockcomedy/PortfolioFragment.kt
@@ -25,7 +25,7 @@ class PortfolioFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_portfolio, container, false)
+        return inflater.inflate(R.layout.fragment_portfolio, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -33,7 +33,6 @@ class PortfolioFragment : Fragment() {
 
         recyclerView = view.findViewById(R.id.portfolioContainer)
         recyclerView.layoutManager = LinearLayoutManager(context)
-
         portfolioAdapter = PortfolioAdapter(portfolioItems)
         recyclerView.adapter = portfolioAdapter
     }
